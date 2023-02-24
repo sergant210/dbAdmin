3650c7bbcf5a8afb10b8054fac61ab2e8a6fad88
Thomas Jakobi <thomas.jakobi@partout.info>
2023-02-24 01:04:56 +0100

    Update to 1.2.0 - Compatibility with MODX 3 - Use <package>.core_path setting to get the table class name - Use GPM 1.0 to build the package for MODX2 and MODX3

---
+++ core/components/dbadmin/lexicon/de/default.inc.php
@@ -0,0 +1,62 @@
+<?php
+/**
+ * Default Lexicon Entries for dbAdmin
+ *
+ * @package dbadmin
+ * @subpackage lexicon
+ */
+$_lang['dbadmin'] = 'dbAdmin';
+$_lang['dbadmin.menu'] = 'dbAdmin';
+$_lang['dbadmin.menu_desc'] = 'Verwaltung von DB-Tabellen';
+$_lang['dbadmin.intro_msg'] = 'Wählen Sie die zu exportierenden, zu kürzenden oder zu löschenden Tabellen aus. Sie können auch eine ganze DB exportieren.';
+$_lang['dbadmin.grid_search'] = 'Suchen';
+$_lang['dbadmin.tables'] = 'Tabellen';
+$_lang['dbadmin.sql'] = 'SQL';
+$_lang['dbadmin.table'] = 'Tabelle';
+$_lang['dbadmin.table_type'] = 'Typ';
+$_lang['dbadmin.table_collation'] = 'Kollation';
+$_lang['dbadmin.table_rows'] = 'Zeilen';
+$_lang['dbadmin.table_size'] = 'Größe, Кb';
+$_lang['dbadmin.table_actions'] = 'Aktionen';
+$_lang['dbadmin.enter_sql_query'] = 'Geben Sie die SQL-Abfrage ein. Anstelle des Tabellennamens können Sie auch eine Klasse in geschweiften Klammern angeben (SELECT * FROM {modUser}).';
+$_lang['dbadmin.class'] = 'Klasse';
+$_lang['dbadmin.package'] = 'Package';
+$_lang['dbadmin.db_export'] = 'Datenbank exportieren';
+$_lang['dbadmin.table_export'] = 'Tabelle exportieren';
+$_lang['dbadmin.selected_export'] = 'Ausgewählte exportieren';
+$_lang['dbadmin.table_remove'] = 'Tabelle entfernen';
+$_lang['dbadmin.tables_remove'] = 'Tabellen entfernen';
+$_lang['dbadmin.row_remove'] = 'Datensatz entfernen';
+$_lang['dbadmin.selected_remove'] = 'Ausgewählte entfernen';
+$_lang['dbadmin.table_remove_confirm'] = 'Sind Sie sicher, dass Sie diese Tabelle löschen wollen?';
+$_lang['dbadmin.tables_remove_confirm'] = 'Sind Sie sicher, dass Sie die ausgewählten Tabellen löschen wollen?';
+$_lang['dbadmin.row_remove_confirm'] = 'Sind Sie sicher, dass Sie den ausgewählten Datensatz löschen möchten?';
+$_lang['dbadmin.table_truncate'] = 'Tabelle kürzen';
+$_lang['dbadmin.tables_truncate'] = 'Tabellen kürzen';
+$_lang['dbadmin.table_truncate_confirm'] = 'Sind Sie sicher, dass Sie diese Tabelle kürzen wollen?';
+$_lang['dbadmin.tables_truncate_confirm'] = 'Sind Sie sicher, dass Sie die ausgewählten Tabellen kürzen wollen?';
+$_lang['dbadmin.selected_truncate'] = 'Ausgewählte kürzen';
+$_lang['dbadmin.table_properties'] = 'Tabelleneigenschaften';
+$_lang['dbadmin.table_view'] = 'Tabelle anzeigen';
+$_lang['dbadmin.table_set_class'] = 'Klasse festlegen';
+$_lang['dbadmin.rows_number'] = 'Gesamt: ';
+$_lang['dbadmin.package_update'] = ' <i class="icon icon-exclamation-triangle package-update" title="dbAdmin-Update ist verfügbar"></i>';
+$_lang['dbadmin.execute'] = 'Ausführen';
+$_lang['dbadmin.clear'] = 'Leeren';
+$_lang['dbadmin.output_type'] = 'Ausgabetyp';
+$_lang['dbadmin.createdon'] = 'Erstellt am: ';
+$_lang['dbadmin.table_structure'] = 'Tabellenstruktur';
+$_lang['dbadmin.table_dump'] = 'Datendump';
+$_lang['dbadmin.table_err_ae'] = 'Eine Tabelle mit demselben Namen existiert bereits!';
+$_lang['dbadmin.table_err_nf'] = 'Die Tabelle wurde nicht gefunden!';
+$_lang['dbadmin.table_err_ns'] = 'Es wurde keine Tabelle ausgewählt!';
+$_lang['dbadmin.table_err_path'] = 'Der Pfad wurde nicht gefunden!';
+$_lang['dbadmin.table_err_save'] = 'Fehler beim Speichern der Tabelle!';
+$_lang['dbadmin.sql_executed_success'] = 'Die Abfrage war erfolgreich.';
+$_lang['dbadmin.sql_executed_failed'] = 'Die Abfrage ist fehlgeschlagen!';
+$_lang['dbadmin.table_err_rename'] = 'Fehler beim Umbenennen der Tabelle!';
+$_lang['dbadmin.sync'] = 'Synchronisationsfehler!';
+$_lang['dbadmin.invalid_data'] = 'Ungültige Daten!';
+$_lang['dbadmin.no_package'] = 'Es wurde kein Package angegeben!';
+// log
+$_lang['dbadmin.err_path'] = '[dbAdmin] Der Pfad für das angegebene Package wurde nicht gefunden!';
---
+++ core/components/dbadmin/lexicon/de/permissions.inc.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Permissions Lexicon Entries for dbAdmin
+ *
+ * @package dbadmin
+ * @subpackage lexicon
+ */
+$_lang['dbadmin.permission.tables_list_desc'] = 'Anzeigen einer Liste von Tabellen erlauben.';
+$_lang['dbadmin.permission.table_view_desc'] = 'Anzeigen einer Tabellen erlauben.';
+$_lang['dbadmin.permission.table_save_desc'] = 'Speichern einer Tabelle erlauben.';
+$_lang['dbadmin.permission.table_truncate_desc'] = 'Kürzen einer Tabelle erlauben.';
+$_lang['dbadmin.permission.table_remove_desc'] = 'Entfernen einer Tabelle erlauben.';
+$_lang['dbadmin.permission.table_export_desc'] = 'Exportieren einer Tabelle erlauben.';
+$_lang['dbadmin.permission.sql_query_execute_desc'] = 'Ausführen von SQL-Abfragen erlauben.';
